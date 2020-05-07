d8f25715d9bcc36f13ec81ac821b1e3f31d7eb96
Benjamin Roycraft <roycraft.benjamin@gmail.com>
2020-05-06 19:18:14 -0700

    update

--- temp.c
+++ temp.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "vector.h"
 
-DECLARE_VEC_TYPE_AND_FUNCS(int, int, unsigned int, 4, 2)
+DECLARE_VEC_TYPE_AND_FUNCS(uint, unsigned int, unsigned int, 4, 2)
 
 int main(void){
 
-  int_Vec vec;
-  init_int_vec(&vec);
+  uint_Vec vec;
+  init_uint_vec(&vec);
+  
+  unsigned int arry[30];
 
+  for(int i = 0; i< 30; i++){
+    arry[i] = i;
+  }
+  
+  app_uint_to_vec(arry, &vec, 30);
+  
+  
+  for(int i = 0; i< 30; i++){
+    printf("%d", vec.init[i]);
+  }
+  printf("\n");
+  
+  free_uint_vec(&vec);
+  
   return 0;
 }
